e7c8e424ca7706db61d51aac61c79e008f078515
Homayoun Norozy <homayounn77@gmail.com>
2026-09-13 01:08:18 +0330

    added filter by category

--- js/app.js
+++ js/app.js
@@ -1,17 +1,52 @@
 const searchInput = document.getElementById("search-input")
 const products = document.querySelectorAll(".product-item")
+const buttons = document.querySelectorAll(".filter")
+
+const changeClass = (filter) => {
+    buttons.forEach(button => {
+        if(button.dataset.filter === filter){
+            button.classList.add("selected")
+        }else{
+            button.classList.remove("selected")
+        }
+    })
+}
 
 const searchHandler = event => {
     const searchValue = event.target.value.toLowerCase().trim()
-    
+
     products.forEach((product) => {
         const productName = product.children[1].innerText.toLowerCase()
-        if(productName.includes(searchValue)){
+        if (productName.includes(searchValue)) {
             product.style.display = "block"
-        }else{
+        } else {
             product.style.display = "none"
         }
     })
 }
 
-searchInput.addEventListener("keyup" , searchHandler)
+const filterHandler = (event) => {
+    const filter = event.target.dataset.filter
+    changeClass(filter)
+
+    products.forEach(product => {
+        const category = product.dataset.category
+        if (filter === "all") {
+            product.style.display = "block"
+        } else {
+            category === filter
+                ? product.style.display = "block"
+                : product.style.display = "none"
+            // if(category === filter){
+            //     product.style.display = "block"
+            // }else{
+            //     product.style.display = "none"
+            // }
+        }
+    })
+}
+
+searchInput.addEventListener("keyup", searchHandler)
+buttons.forEach(button => {
+    button.addEventListener("click", filterHandler)
+})
